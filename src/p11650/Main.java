007e72dfbddc5d377adc77c71bbd18ee4164851f
ch02111 <ch0211108@gmail.com>
2025-12-15 09:31:24 +0900

    20251215 commit

--- src/p11650/Main.java
+++ src/p11650/Main.java
@@ -3,13 +3,14 @@ package p11650;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
         int cases = Integer.parseInt(br.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
-        
+
+        System.out.println(array.toString());
     }
 }
